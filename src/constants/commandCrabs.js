75309ed302accae021f5b737a49f6ed8739c9e16
Perry512 <perry512@hotmail.com>
2025-05-06 19:45:35 -0400

    [ADD] Base: Created enum list for some of the command grabs of the day

---
+++ src/constants/commandCrabs.js
@@ -0,0 +1,139 @@
+export const COMMANDCRABS = Object.freeze ({
+    "GRABS": [
+        {
+            "grabName":"?",
+            "grabCharacter":"?",
+            "grabGameOrigin":"?",
+            "grabInput":"Full Circle + Punch",
+        },
+        {
+            "grabName":"Spinning Pile Driver",
+            "grabCharacter":"Zangeif", 
+            "grabGameOrigin":"Street Fighter II",
+            "grabInput":"360P",
+        },
+        {
+            "grabName":"Mexican Typhoon",
+            "grabCharacter":"T. Hawk",
+            "grabGameOrigin":"Super Steet Fighter II", 
+            "grabInput":"Full Circle + Punch" 
+        },
+        {
+            "grabName":"Kill Process", 
+            "grabCharacter":"Robot", 
+            "grabGameOrigin":"Your Only Move Is Hustle", 
+            "grabInput":"Unique" 
+        },
+        {
+            "grabName":"Rock the Baby", 
+            "grabCharacter":"Bridget", 
+            "grabGameOrigin":"Guilty Gear Strive", 
+            "grabInput":"624P" 
+        },
+        {
+            "grabName":"Wild Throw", 
+            "grabCharacter":"Sol Badguy", 
+            "grabGameOrigin":"Guilty Gear Accent Core", 
+            "grabInput":"623K" 
+        },
+        {
+            "grabName":"Sunset Wheel", 
+            "grabCharacter":"Laura", 
+            "grabGameOrigin":"Street Fighter V", 
+            "grabInput":"Half-circle back + Kick" 
+        },
+        {
+            "grabName":"Meat Squasher", 
+            "grabCharacter":"Meat Squasher", 
+            "grabGameOrigin":"Street Fighter III: 2nd Impact", 
+            "grabInput":"Full Circle + Punch" 
+        },
+        {
+            "grabName":"Colossal Crush", 
+            "grabCharacter":"Talos", 
+            "grabGameOrigin":"Rising Thunder", 
+            "grabInput":"Unique" 
+        },
+        {
+            "grabName":"Excellebella", 
+            "grabCharacter":"Cerebella", 
+            "grabGameOrigin":"Skullgirls", 
+            "grabInput":"623LP + LK" 
+        },
+        {
+            "grabName":"Minecart", 
+            "grabCharacter":"Steve", 
+            "grabGameOrigin":"Super Smash Bros. Ultimate", 
+            "grabInput":"Side-b" },
+        {
+            "grabName":"Overhead Kiss", 
+            "grabCharacter":"May", 
+            "grabGameOrigin":"Guilty Gear Strive", 
+            "grabInput":"623K" 
+        },
+        {
+            "grabName":"The Shape of Love", 
+            "grabCharacter":"Ladiva", 
+            "grabGameOrigin":"Granblue Fantasy: Versus", 
+            "grabInput":"720U (Air OK)" },
+        {
+            "grabName":"Gigantic Claw", 
+            "grabCharacter":"Broly", 
+            "grabGameOrigin":"Dragon Ball: FighterZ", 
+            "grabInput":"214 L/M/H" },
+        {
+            "grabName":"Might Hurricane", 
+            "grabCharacter":"Thor", 
+            "grabGameOrigin":"Ultimate Marvel Versus Capcom 3", 
+            "grabInput":"Full Circle + Attack (Air OK)" 
+        },
+        {
+            "grabName":"Savvy Ninpo: Tax Write-off",
+            "grabCharacter":"Answer",
+            "grabGameOrigin":"Guilty Gear: Xrd",
+            "grabInput":"623K (Air OK)",
+        },
+        {
+            "grabName":"Rolling Death Cradle",
+            "grabCharacter":"King",
+            "grabGameOrigin":"Tekken 3",
+            "grabInput":"1+3, 3+4, 2+4, 1+2, 1+2+3",
+        },
+        {
+            "grabName":"Winter Mantis",
+            "grabCharacter":"Axl Low",
+            "grabGameOrigin":"GGX",
+            "grabInput":"[4]6 + HS",
+        },
+        {
+            "grabName":"Lasso",
+            "grabCharacter":"Arizona",
+            "grabGameOrigin":"Thems Fightin' Heards",
+            "grabInput":"5D",
+        },
+        {
+            "grabName":"King of Tager",
+            "grabCharacter":"Iron Tager",
+            "grabGameOrigin":"BlazBlue",
+            "grabInput":"1080D",
+        },
+        {
+            "grabName":"Alolan Whip",
+            "grabCharacter":"Incineroar",
+            "grabGameOrigin":"Pokemon Sun & Moon",
+            "grabInput":"Side-b -> b",
+        },
+        {
+            "grabName":"Tombstone Piledriver",
+            "grabCharacter":"'02 Undertaker",
+            "grabGameOrigin":"WWE 2k22",
+            "grabInput":"???",
+        },
+        {
+            "grabName":"Take the A'Train",
+            "grabCharacter":"Big Band",
+            "grabGameOrigin":"Skullgirls",
+            "grabInput":"[4]6 MK",
+        },
+    ]
+})
